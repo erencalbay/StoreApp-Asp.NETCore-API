31a34f35bdfa4f0fbeb3d6ea654c19f31f869780
Eren Çalbay <erencalbayfb@hotmail.com>
2023-12-06 23:05:45 +0300

    Feat: MSSQL connection enabled, migrations created.

---
+++ WebAPI/Repositories/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options):
+            base(options)
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
